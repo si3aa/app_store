4d5bd81bb98069f7469373728e104050d924f414
si3aa <mahmoud22said22@gmail.com>
2024-07-20 16:18:06 +0300

    check statusCode

--- lib/services/category_services.dart
+++ lib/services/category_services.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
+
 import 'package:app_store/models/product_model.dart';
 import 'package:http/http.dart' as http;
 
-class AllProductsServices {
-  Future<List<ProductModel>> getAllProducts() async {
-    http.Response response =
-        await http.get(Uri.parse('https://fakestoreapi.com/products'));
+class CategoryServices {
+  Future<List<ProductModel>> getCategoriesProduct(
+      {required String categoryName}) async {
+    http.Response response = await http.get(
+        Uri.parse('https://fakestoreapi.com/products/category/$categoryName'));
     if (response.statusCode == 200) {
       List<dynamic> data = jsonDecode(response.body);
       List<ProductModel> productsList = [];
