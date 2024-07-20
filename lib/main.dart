a89be2f4aec4f58f8a6046bd84ff3a08cb60fcc7
si3aa <mahmoud22said22@gmail.com>
2024-07-20 14:39:01 +0300

    upload project folders

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const AppStore());
+}
+
+class AppStore extends StatelessWidget {
+  const AppStore({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      
+    );
+  }
+}
+
