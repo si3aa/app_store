c84224dd9f16d2a4f88af4eb4b57f2e2ed8a3845
si3aa <mahmoud22said22@gmail.com>
2024-07-23 04:07:07 +0300

    create product card

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -29,6 +29,57 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      body: Container(
+        height: 130,
+        width: 200,
+        decoration: BoxDecoration(boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(.2),
+            blurRadius: 40,
+            spreadRadius: 0,
+            offset: const Offset(10, 10),
+          )
+        ]),
+        child: const Card(
+          elevation: 10,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16,vertical: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'HandBag lv',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      r'$225',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.red,
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
